f3288b94669af5af26d334d16779c3e3def48218
Chan Baek <z5259628@ad.unsw.edu.au>
2021-10-22 03:48:11 +1100

    WIP Milestone 2

--- frontend/src/channels.js
+++ frontend/src/channels.js
@@ -5,18 +5,21 @@ export function updateChannelShow(TOKEN) {
 	.then((data) => {
 		data['channels'].map(channels => {
 			if (document.getElementsByName(channels['name']).length === 0) {
-				addChannelButton(channels['name'], channels['private']);
+				addChannelButton(channels['name'], channels['private'], TOKEN, channels['id']);
 			}
 		})
 	})
 }
 
-export function addChannelButton(value, isPrivate) {
+export function addChannelButton(value, isPrivate, TOKEN, channelId) {
 	var newline = document.createElement("br");
     var element = document.createElement("input");
     element.type = "button";
 	element.value = value;
 	element.name = value;
+	element.onclick = function() {
+		updateChannelScreen(TOKEN, channelId);
+	};
 
 	if (isPrivate) {
 		var priv = document.getElementById("privateChannelList");
@@ -41,5 +44,28 @@ export function createChannel(TOKEN) {
 	helper.myFetch('POST', 'channel', TOKEN, body)
 	.then(data => {
 		updateChannelShow(TOKEN);
+	})
+}
+
+export function updateChannelScreen(TOKEN, channelId) {
+	helper.myFetch('GET', `channel/${channelId}`, TOKEN)
+	.then(data => {
+		var date = new Date(data['createdAt']);
+		let newString = `Channel Name: ${data['name']}\n
+		Description: ${data['description']}\n
+		Private: ${data['private']}\n
+		Created at: ${date}\n`
+		document.getElementById("infoText").textContent = newString
+		return helper.myFetch("GET", `user/${data['creator']}`, TOKEN);
+	})
+	.then(data => {
+		document.getElementById("infoText").textContent += `\nCreator: ${data['name']}`;
+		return helper.myFetch('GET', `message/${channelId}?start=0`, TOKEN);
+	})
+	.then(data => {
+		document.getElementById("messageText").textContent = '';
+		for (let i=0; i < data['messages'].length; i++) {
+			document.getElementById("messageText").textContent += `${data['messages'][i]['message']}\n`;
+		}
 	})
 }
